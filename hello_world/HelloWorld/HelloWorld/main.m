c6c15f1c80cfb75ee7e9366ad020ab30388ee2f6
Max Demian <mikar@gmx.de>
2015-03-29 21:51:03 +0200

    commit test

--- hello_world/HelloWorld/HelloWorld/main.m
+++ hello_world/HelloWorld/HelloWorld/main.m
@@ -9,6 +9,7 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSString *s1 = @"Hello ";
         NSString *s2 = @"World!";
+        
         NSLog([s1 stringByAppendingString:s2]);
     }
     return 0;
