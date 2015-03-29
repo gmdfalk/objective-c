4556dd299a699dd3f9c0a3cb98cabb0b7772ba2f
Max Demian <mikar@gmx.de>
2015-03-29 21:35:40 +0200

    asdf

---
+++ hello_world/Test/Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Falk, Geronimo on 29/03/2015.
+//  Copyright (c) 2015 Falk, Geronimo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
