dd87c4b9ac764224ae3e882d3ad15165a1c0f975
Max Demian <mikar@gmx.de>
2015-05-21 18:19:58 +0000

    codechef

---
+++ exercises/codechef/codechef/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  codechef
+//
+//  Created by Max on 23/05/15.
+//  Copyright (c) 2015 Max. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
