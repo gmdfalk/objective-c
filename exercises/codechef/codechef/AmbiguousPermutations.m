20c813b575efb7d79bf74c9942dcca7ee9953e1b
Max Demian <mikar@gmx.de>
2015-05-24 00:01:04 +0000

    inverse permutations

--- exercises/codechef/codechef/AmbiguousPermutations.m
+++ exercises/codechef/codechef/AmbiguousPermutations.m
@@ -1,7 +1,8 @@
 //
 //  AmbiguousPermutations.m
 //  codechef
-//
+
+// http://www.codechef.com/problems/PERMUT2
 //  Created by Max on 23/05/15.
 //  Copyright (c) 2015 Max. All rights reserved.
 //
@@ -18,8 +19,17 @@
 
 + (NSArray *)createInversePermutation:(NSArray *)nums {
     NSMutableArray *result = [NSMutableArray new];
+    int swappedPosition;
+    int currentPosition = 0;
+    // Initialize empty array.
     for (int i = 0; i < nums.count; i++) {
-        [result addObject:nums[[nums[i] intValue] - 1]];
+        [result addObject:[NSNull null]];
+    }
+    // Inverse positions.
+    for (NSString *num in nums) {
+        currentPosition++;
+        swappedPosition = [num intValue];
+        [result replaceObjectAtIndex:swappedPosition - 1 withObject:[NSString stringWithFormat:@"%d", currentPosition]];
     }
     return result;
 }
